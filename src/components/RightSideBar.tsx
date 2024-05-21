7ced37c4f40492d89eb9314b993f8068e3f97ed4
Thai Huynh <rafehuynh0510@gmail.com>
2024-05-21 16:27:51 -0500

    added spending sumarize, pagination components

--- src/components/RightSideBar.tsx
+++ src/components/RightSideBar.tsx
@@ -3,11 +3,14 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import BankCards from './BankCards'
+import { countTransactionCategories } from '../../lib/utils'
+import Category from './Category'
 const RightSideBar = ({user, transactions, banks}: RightSidebarProps) => {
+    const categories: CategoryCount[] = countTransactionCategories(transactions)
   return (
     <aside className='right-sidebar'>
         <section className='flex flex-col pb-8'>
-            <div className='profile-banner'>
+            <div className='profile-banner'/>
                 <div className='profile'>
                     <div className='profile-img'>
                         <span className='text-5xl font-bold text-blue-500'>
@@ -23,7 +26,7 @@ const RightSideBar = ({user, transactions, banks}: RightSidebarProps) => {
                         </p>
                     </div>
                 </div>
-            </div>
+            
         </section>
         <section className='banks'>
             <div className='flex w-full justify-between'>
@@ -50,12 +53,20 @@ const RightSideBar = ({user, transactions, banks}: RightSidebarProps) => {
                             </div>
                         )}
                     </div>
+                )    
+            }
+            <div className='mt-10 flex flex-1 flex-col gap-6'>
+                <h2 className='header-2'>
+                Spending Sumarize
+                </h2>
+                <div className='space-y-5'>
+                {categories.map((category, index) => (
+                    <Category key={category.name} category ={category} />
                 )
 
-                
-
-                
-            }
+                )}
+                </div>
+            </div>
         </section>
     </aside>
   )
